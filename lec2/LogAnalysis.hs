bb1469a362f3cb9d8e3071fa68b589d386710287
hugh <alicorn0618@outlook.com>
2023-12-17 16:47:47 +0800

    update

--- lec2/LogAnalysis.hs
+++ lec2/LogAnalysis.hs
@@ -1,3 +1,17 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+
+import Log
+
 -- 'I' for information messages
 -- 'W' for warnings, and
 -- 'E' for errors
+
+-- Error message lines then have 1 integer indicating the severity of the error
+-- with 1 being the sort of error you might get around to caring about some time
+
+-- All types of log messages then have an integer time stamp followed by textual
+-- content that runs to the end of the line
+
+parseMessage :: String -> LogMessage
